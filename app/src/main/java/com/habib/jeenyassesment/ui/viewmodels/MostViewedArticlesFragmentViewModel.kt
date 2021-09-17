e8e9547245bd0f9961a4e4a094e0512fdfda97d2
Habib ur rehman <habib@gordiansol.com>
2021-09-17 20:44:22 +0500

    build, viewmodel completion, layout design mostViewedArticles fragment, start with item view

--- app/src/main/java/com/habib/jeenyassesment/ui/viewmodels/MostViewedArticlesFragmentViewModel.kt
+++ app/src/main/java/com/habib/jeenyassesment/ui/viewmodels/MostViewedArticlesFragmentViewModel.kt
@@ -1,13 +1,69 @@
 package com.habib.jeenyassesment.ui.viewmodels
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.habib.jeenyassesment.repository.Repository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.io.IOException
 import javax.inject.Inject
 
 @HiltViewModel
 class MostViewedArticlesFragmentViewModel @Inject constructor(
     private val repository: Repository
 ): ViewModel(){
+
+    // update the UI from the db
+    val mostViewed = repository.dataItem
+    /**
+     * Event triggered for network error. This is private to avoid exposing a
+     * way to set this value to observers.
+     */
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+    /**
+     * Event triggered for network error. Views should use this to get access
+     * to the data.
+     */
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+    /**
+     * Flag to display the error message. This is private to avoid exposing a
+     * way to set this value to observers.
+     */
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+    /**
+     * Flag to display the error message. Views should use this to get access
+     * to the data.
+     */
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+
+    init {
+        refreshDataFromRepository()
+    }
+
+    private fun refreshDataFromRepository() {
+        viewModelScope.launch {
+            try {
+                repository.refreshMostViewed(1)
+                _eventNetworkError.value = false
+                _isNetworkErrorShown.value = false
+
+            } catch (networkError: IOException) {
+                // Show a Toast error message and hide the progress bar.
+                if(mostViewed.value.isNullOrEmpty())
+                    _eventNetworkError.value = true
+            }
+        }
+    }
+
+    /**
+     * Resets the network error flag.
+     */
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
 
 }
