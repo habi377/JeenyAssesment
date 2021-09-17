b827571a3c26421fdf788f43eb273f3e4f088ede
Habib ur rehman <habib@gordiansol.com>
2021-09-17 19:21:57 +0500

    Retrofit Interface, add API Key, dependency for room

--- app/src/main/java/com/habib/jeenyassesment/data/remote/NYtimesAPI.kt
+++ app/src/main/java/com/habib/jeenyassesment/data/remote/NYtimesAPI.kt
@@ -1,4 +1,21 @@
 package com.habib.jeenyassesment.data.remote
 
+import com.habib.jeenyassesment.BuildConfig
+import com.habib.jeenyassesment.data.remote.models.BaseModel
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
 interface NYtimesAPI {
+
+    companion object {
+        private const val PARAM_PERIOD = "period"
+        private const val MOST_VIEWED = "/svc/mostpopular/v2/viewed/{$PARAM_PERIOD}.json"
+    }
+
+    @GET(MOST_VIEWED)
+    suspend fun getMostViewed(
+        @Path(PARAM_PERIOD) period : Int,
+        @Query("api-key") apiKey : String = BuildConfig.API_KEY
+    ): BaseModel
 }
